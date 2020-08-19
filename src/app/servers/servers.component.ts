289d79277a97827526801d2e73d7bf7ec18ad849
Engincan Varan <engincanvaran@Engincans-MacBook-Pro.local>
2020-08-19 17:18:47 +0300

    Component - Custom / Styling, Selector, Templates

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers', // value
+  // selector: '[app-servers]',  // attribute 
+  // selector: '.app-servers',   // class
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
